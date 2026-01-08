caea08a03c8b4c8fceb2ae85e82e184420d5295b
Rimsha <rimsharamzan1247@gmail.com>
2026-01-08 05:03:19 -0700

    init

--- script.js
+++ script.js
@@ -38,3 +38,17 @@ video.addEventListener('timeupdate', () => {
         video.style.opacity = '0.3'
     }
 })
+//  piricng cards
+ var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
